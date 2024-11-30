ecd33e17767d043b2dd98f023772975ac33c5919
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2024-11-29 20:52:45 -0800

    Adds changes to the Index.html - Style.css files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Figure out text later";
 
 document.querySelector("body").appendChild(h2);
